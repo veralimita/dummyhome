62ecae40ed58e609d5234bd46407382754e3ff6b
veralimita <vera.limita@gmail.com>
2016-10-13 23:07:45 +0200

    gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,37 @@
 // Gruntfile.js
-module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-  });
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		less: {
+			options: {
+				banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+			},
+			build: {
+				files: {
+					'tmp/app.css': 'styles/app.less'
+				}
+			}
+		},
+		cssmin: {
+			options: {
+				banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+			},
+			build: {
+				files: {
+					'public/app.min.css': 'tmp/app.css'
+				}
+			}
+		},
+		watch: {
+			files: ['styles/*.less'],
+			tasks: ['less', 'cssmin']
+		}
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
+
+	grunt.registerTask('default', ['less', 'cssmin']);
 };
